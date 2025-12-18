8ef0d5677fdcc259b9d45bc2b968adaa0a0b97fb
Ryan Atkinson <mail@ryanatkn.com>
2025-12-18 10:03:55 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import {defineConfig} from 'vite';
 import {sveltekit} from '@sveltejs/kit/vite';
 
+import {vite_plugin_well_known} from './src/lib/vite_plugin_well_known.ts';
+
 export default defineConfig(({mode}) => ({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), vite_plugin_well_known()],
 	// In test mode, use browser conditions so Svelte's mount() resolves to the client version
 	resolve: mode === 'test' ? {conditions: ['browser']} : undefined,
 }));
