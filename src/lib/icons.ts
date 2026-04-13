1b4a7a338e0b0bd54e29a300384528fe6207c8ac
Ryan Atkinson <mail@ryanatkn.com>
2026-04-13 14:44:35 -0400

    wip

---
+++ src/lib/icons.ts
@@ -0,0 +1,616 @@
+import type {SvgData} from './Svg.svelte';
+
+// SVG icon data for replacing character glyphs with scalable vector icons.
+// Each export uses the `icon_` prefix and conforms to `SvgData`.
+// Default viewBox is "0 0 100 100".
+
+// --- Status ---
+
+export const icon_unknown = {
+	label: 'unknown, double question mark',
+	paths: [
+		{
+			d: 'M22 25a13 13 0 0 1 13-13 13 13 0 0 1 13 13c0 9-8 11-8 20h-10c0-13 8-13 8-20a3 3 0 0 0-3-3 3 3 0 0 0-3 3h-10zm8 28h10v10h-10zm22-28a13 13 0 0 1 13-13 13 13 0 0 1 13 13c0 9-8 11-8 20h-10c0-13 8-13 8-20a3 3 0 0 0-3-3 3 3 0 0 0-3 3h-10zm8 28h10v10h-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_important = {
+	label: 'important, question and exclamation',
+	paths: [
+		{
+			d: 'M20 25a13 13 0 0 1 13-13 13 13 0 0 1 13 13c0 9-8 11-8 20h-10c0-13 8-13 8-20a3 3 0 0 0-3-3 3 3 0 0 0-3 3h-10zm8 28h10v10h-10zm27-41h10v42h-10zm0 50h10v10h-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_info = {
+	label: 'info circle',
+	paths: [
+		{
+			d: 'M50 5a45 45 0 1 0 0 90 45 45 0 0 0 0-90zm0 10a35 35 0 1 1 0 70 35 35 0 0 1 0-70zm-5 15h10v10h-10zm0 15h10v25h-10z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Actions ---
+
+export const icon_add = {
+	label: 'add, plus',
+	paths: [{d: 'M45 15v30h-30v10h30v30h10v-30h30v-10h-30v-30z'}],
+} satisfies SvgData;
+
+export const icon_remove = {
+	label: 'remove, x mark',
+	paths: [
+		{
+			d: 'M26.5 19.4l-7.1 7.1L42.9 50 19.4 73.5l7.1 7.1L50 57.1l23.5 23.5 7.1-7.1L57.1 50l23.5-23.5-7.1-7.1L50 42.9z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_delete = {
+	label: 'delete, x in square',
+	paths: [
+		{
+			d: 'M10 10h80v80h-80zm10 10v60h60v-60zm8 8l7-7 15 15 15-15 7 7-15 15 15 15-7 7-15-15-15 15-7-7 15-15z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_clear = {
+	label: 'clear, backspace',
+	paths: [
+		{
+			d: 'M35 15l-25 35 25 35h55v-70zm10 15l7-7 13 13 13-13 7 7-13 13 13 13-7 7-13-13-13 13-7-7 13-13z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_restore = {
+	label: 'restore, redo arrow',
+	paths: [
+		{
+			d: 'M65 20l20 20-20 20v-12a30 30 0 0 0-30 30h-12a42 42 0 0 1 42-42z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_cancel = {
+	label: 'cancel, circle with line',
+	paths: [
+		{
+			d: 'M50 5a45 45 0 1 0 0 90 45 45 0 0 0 0-90zm0 10a35 35 0 0 1 24.7 59.7L25.3 25.3A35 35 0 0 1 50 15zm0 70a35 35 0 0 1-24.7-59.7l49.4 49.4A35 35 0 0 1 50 85z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_drag = {
+	label: 'drag handle, three horizontal lines',
+	paths: [{d: 'M20 25h60v8h-60zm0 17h60v8h-60zm0 17h60v8h-60z'}],
+} satisfies SvgData;
+
+export const icon_copy = {
+	label: 'copy, overlapping squares',
+	paths: [
+		{
+			d: 'M25 10h45a5 5 0 0 1 5 5v45a5 5 0 0 1-5 5h-45a5 5 0 0 1-5-5v-45a5 5 0 0 1 5-5zm5 10v35h35v-35zm-5 20h-10v45a5 5 0 0 0 5 5h45v-10h-35a5 5 0 0 1-5-5z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_paste = {
+	label: 'paste, clipboard',
+	paths: [
+		{
+			d: 'M35 5h30v10h15a5 5 0 0 1 5 5v65a5 5 0 0 1-5 5h-60a5 5 0 0 1-5-5v-65a5 5 0 0 1 5-5h15zm5 5v10h20v-10zm-15 10v60h50v-60h-10v10h-30v-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_reset = {
+	label: 'reset, undo arrow',
+	paths: [
+		{
+			d: 'M35 20l-20 20 20 20v-12a30 30 0 0 1 30 30h12a42 42 0 0 0-42-42z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_refresh = {
+	label: 'refresh, circular arrow',
+	paths: [
+		{
+			d: 'M50 10a40 40 0 0 1 40 40h-12a28 28 0 0 0-28-28 28 28 0 0 0-28 28 28 28 0 0 0 28 28v12a40 40 0 0 1-40-40 40 40 0 0 1 40-40zm28 45l17 17-17 17z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_connect = {
+	label: 'connect, link',
+	paths: [
+		{
+			d: 'M30 40a15 15 0 0 0-15 15 15 15 0 0 0 15 15h10v10h-10a25 25 0 0 1-25-25 25 25 0 0 1 25-25h10v10zm30-10h10a25 25 0 0 1 25 25 25 25 0 0 1-25 25h-10v-10h10a15 15 0 0 0 15-15 15 15 0 0 0-15-15h-10zm-25 20h30v10h-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_disconnect = {
+	label: 'disconnect, broken link',
+	paths: [
+		{
+			d: 'M30 40a15 15 0 0 0-15 15 15 15 0 0 0 15 15h10v10h-10a25 25 0 0 1-25-25 25 25 0 0 1 25-25h10v10zm30-10h10a25 25 0 0 1 25 25 25 25 0 0 1-25 25h-10v-10h10a15 15 0 0 0 15-15 15 15 0 0 0-15-15h-10zm-22 15l-8-15h10l8 15zm24 10l8 15h-10l-8-15z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_retry = {
+	label: 'retry, counterclockwise arrow',
+	paths: [
+		{
+			d: 'M50 10a40 40 0 0 0-40 40h12a28 28 0 0 1 28-28 28 28 0 0 1 28 28 28 28 0 0 1-28 28v12a40 40 0 0 0 40-40 40 40 0 0 0-40-40zm-28 45l-17 17 17 17z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_placeholder = {
+	label: 'placeholder, return arrow',
+	paths: [{d: 'M75 15v40h-40v15l-25-20 25-20v15h30v-30z'}],
+} satisfies SvgData;
+
+export const icon_send = {
+	label: 'send, paper plane',
+	paths: [{d: 'M10 85l80-35-80-35v27l55 8-55 8z'}],
+} satisfies SvgData;
+
+// --- Media ---
+
+export const icon_play = {
+	label: 'play',
+	paths: [{d: 'M25 10l55 40-55 40z'}],
+} satisfies SvgData;
+
+export const icon_pause = {
+	label: 'pause',
+	paths: [{d: 'M25 15h15v70h-15zm35 0h15v70h-15z'}],
+} satisfies SvgData;
+
+// --- Validation ---
+
+export const icon_checkmark = {
+	label: 'checkmark',
+	paths: [{d: 'M15 52l10-10 17 17 33-33 10 10-43 43z'}],
+} satisfies SvgData;
+
+export const icon_xmark = {
+	label: 'x mark',
+	paths: [
+		{
+			d: 'M26 19l-7 7 24 24-24 24 7 7 24-24 24 24 7-7-24-24 24-24-7-7-24 24z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_xmark_heavy = {
+	label: 'heavy x mark',
+	paths: [
+		{
+			d: 'M24 14l-10 10 26 26-26 26 10 10 26-26 26 26 10-10-26-26 26-26-10-10-26 26z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Navigation ---
+
+export const icon_download = {
+	label: 'download arrow',
+	paths: [{d: 'M45 10v40h-20l25 30 25-30h-20v-40zm-30 70v10h70v-10z'}],
+} satisfies SvgData;
+
+export const icon_error = {
+	label: 'error, double exclamation',
+	paths: [
+		{
+			d: 'M27 10h10v45h-10zm0 55h10v10h-10zm36-55h10v45h-10zm0 55h10v10h-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_chevron_up = {
+	label: 'chevron up',
+	paths: [{d: 'M50 25l35 35-10 10-25-25-25 25-10-10z'}],
+} satisfies SvgData;
+
+export const icon_chevron_right = {
+	label: 'chevron right',
+	paths: [{d: 'M35 15l35 35-35 35-10-10 25-25-25-25z'}],
+} satisfies SvgData;
+
+export const icon_chevron_down = {
+	label: 'chevron down',
+	paths: [{d: 'M50 75l-35-35 10-10 25 25 25-25 10 10z'}],
+} satisfies SvgData;
+
+export const icon_chevron_left = {
+	label: 'chevron left',
+	paths: [{d: 'M65 85l-35-35 35-35 10 10-25 25 25 25z'}],
+} satisfies SvgData;
+
+export const icon_double_chevron_left = {
+	label: 'double chevron left',
+	paths: [
+		{
+			d: 'M50 85l-35-35 35-35 8 8-27 27 27 27zm28 0l-35-35 35-35 8 8-27 27 27 27z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_double_chevron_right = {
+	label: 'double chevron right',
+	paths: [
+		{
+			d: 'M22 15l35 35-35 35-8-8 27-27-27-27zm28 0l35 35-35 35-8-8 27-27-27-27z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Editing ---
+
+export const icon_edit = {
+	label: 'edit, pencil',
+	paths: [
+		{
+			d: 'M62 8l20 20-45 45-20-20zm-50 55l-5 20 20-5zm55-65l10-8 8 2 2 8-8 10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_sort = {
+	label: 'sort, up and down arrows',
+	paths: [
+		{
+			d: 'M30 40l-20-25h12v-10h16v10h12zm40 20l20 25h-12v10h-16v-10h-12z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Domain ---
+
+export const icon_chat = {
+	label: 'chat bubble',
+	paths: [
+		{
+			d: 'M15 10h70a5 5 0 0 1 5 5v50a5 5 0 0 1-5 5h-40l-20 20v-20h-10a5 5 0 0 1-5-5v-50a5 5 0 0 1 5-5zm10 10v40h10v12l12-12h38v-40z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_thread = {
+	label: 'thread, broken stacked lines',
+	paths: [
+		{
+			d: 'M15 20h30v10h-30zm40 0h30v10h-30zm-40 20h30v10h-30zm40 0h30v10h-30zm-40 20h30v10h-30zm40 0h30v10h-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_turn = {
+	label: 'turn, speech bracket',
+	paths: [
+		{
+			d: 'M20 15h60v10h-60zm20 20h40v10h-40zm-20-5v35h10v-30h50v-5z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_file = {
+	label: 'file, document',
+	paths: [
+		{
+			d: 'M20 5h40l25 25v60a5 5 0 0 1-5 5h-60a5 5 0 0 1-5-5v-80a5 5 0 0 1 5-5zm5 10v70h50v-50h-20v-20zm30 0v15h15z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_list = {
+	label: 'list, bulleted lines',
+	paths: [
+		{
+			d: 'M15 20a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm12 0h58v8h-58zm-12 18a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm12 0h58v8h-58zm-12 18a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm12 0h58v8h-58zm-12 18a4 4 0 1 0 0 8 4 4 0 0 0 0-8zm12 0h58v8h-58z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_directory = {
+	label: 'directory, folder',
+	paths: [
+		{
+			d: 'M10 15h30l10 10h40a5 5 0 0 1 5 5v50a5 5 0 0 1-5 5h-80a5 5 0 0 1-5-5v-60a5 5 0 0 1 5-5zm5 10v50h70v-40h-40l-10-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_create_file = {
+	label: 'create file, document with plus',
+	paths: [
+		{
+			d: 'M20 5h40l25 25v60a5 5 0 0 1-5 5h-60a5 5 0 0 1-5-5v-80a5 5 0 0 1 5-5zm5 10v70h50v-50h-20v-20zm15 30h8v10h10v8h-10v10h-8v-10h-10v-8h10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_create_folder = {
+	label: 'create folder, folder with plus',
+	paths: [
+		{
+			d: 'M10 15h30l10 10h40a5 5 0 0 1 5 5v50a5 5 0 0 1-5 5h-80a5 5 0 0 1-5-5v-60a5 5 0 0 1 5-5zm5 10v50h70v-40h-40l-10-10zm25 15h10v10h10v10h-10v10h-10v-10h-10v-10h10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_prompt = {
+	label: 'prompt, chevron with cursor',
+	paths: [{d: 'M10 25l30 25-30 25-8-10 18-15-18-15zm35 40h40v10h-40z'}],
+} satisfies SvgData;
+
+export const icon_part = {
+	label: 'part, dashed vertical line',
+	paths: [
+		{
+			d: 'M45 10h10v15h-10zm0 22h10v15h-10zm0 22h10v15h-10zm0 22h10v15h-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_provider = {
+	label: 'provider, circle with plus',
+	paths: [
+		{
+			d: 'M50 5a45 45 0 1 0 0 90 45 45 0 0 0 0-90zm0 10a35 35 0 1 1 0 70 35 35 0 0 1 0-70zm-5 15h10v15h15v10h-15v15h-10v-15h-15v-10h15z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_model = {
+	label: 'model, asterisk in circle',
+	paths: [
+		{
+			d: 'M50 5a45 45 0 1 0 0 90 45 45 0 0 0 0-90zm0 10a35 35 0 1 1 0 70 35 35 0 0 1 0-70zm-4 15h8v12l10-6 4 7-11 7 11 7-4 7-10-6v12h-8v-12l-10 6-4-7 11-7-11-7 4-7 10 6z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_action = {
+	label: 'action, lightning bolt',
+	paths: [{d: 'M55 5l-30 45h20l-5 45 35-50h-20z'}],
+} satisfies SvgData;
+
+export const icon_view = {
+	label: 'view, eye',
+	paths: [
+		{
+			d: 'M50 20c-25 0-42 30-42 30s17 30 42 30 42-30 42-30-17-30-42-30zm0 10a20 20 0 1 1 0 40 20 20 0 0 1 0-40zm0 8a12 12 0 1 0 0 24 12 12 0 0 0 0-24z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_preview = {
+	label: 'preview, concentric circles',
+	paths: [
+		{
+			d: 'M50 10a40 40 0 1 0 0 80 40 40 0 0 0 0-80zm0 10a30 30 0 1 1 0 60 30 30 0 0 1 0-60zm0 10a20 20 0 1 0 0 40 20 20 0 0 0 0-40zm0 10a10 10 0 1 1 0 20 10 10 0 0 1 0-20z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_log = {
+	label: 'log, scroll',
+	paths: [
+		{
+			d: 'M20 10h50a10 10 0 0 1 10 10v60a10 10 0 0 1-10 10h-50a10 10 0 0 1-10-10v-60a10 10 0 0 1 10-10zm5 15v50h40v-50zm5 8h30v6h-30zm0 14h30v6h-30zm0 14h20v6h-20z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_tab = {
+	label: 'tab, window frame',
+	paths: [
+		{
+			d: 'M10 15h80v70h-80zm8 8v10h25v-10zm0 18v34h64v-34z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_project = {
+	label: 'project, hexagon',
+	paths: [
+		{
+			d: 'M50 5l40 23v44l-40 23-40-23v-44zm0 12l-30 17v34l30 17 30-17v-34z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_capability = {
+	label: 'capability, wrench',
+	paths: [
+		{
+			d: 'M72 8a28 28 0 0 0-25 40l-30 30a8 8 0 0 0 0 11l4 4a8 8 0 0 0 11 0l30-30a28 28 0 0 0 38-29l-14 14-12-4-4-12 14-14a28 28 0 0 0-12-10z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_settings = {
+	label: 'settings, gear',
+	paths: [
+		{
+			d: 'M43 5h14l2 12a35 35 0 0 1 10 6l11-5 7 12-9 8a35 35 0 0 1 0 12l9 8-7 12-11-5a35 35 0 0 1-10 6l-2 12h-14l-2-12a35 35 0 0 1-10-6l-11 5-7-12 9-8a35 35 0 0 1 0-12l-9-8 7-12 11 5a35 35 0 0 1 10-6zm7 30a15 15 0 1 0 0 30 15 15 0 0 0 0-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_domain = {
+	label: 'domain, diamond',
+	paths: [
+		{
+			d: 'M50 5l40 45-40 45-40-45zm0 15l-25 30 25 30 25-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_page = {
+	label: 'page, browser window',
+	paths: [
+		{
+			d: 'M10 10h80v80h-80zm8 8v12h64v-12zm0 20v44h64v-44z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Space/Container ---
+
+export const icon_space = {
+	label: 'space, house',
+	paths: [
+		{
+			d: 'M50 10l40 35h-12v40h-20v-25h-16v25h-20v-40h-12zm0 18l-20 17v30h8v-25h24v25h8v-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_desk = {
+	label: 'desk, star in circle',
+	paths: [
+		{
+			d: 'M50 5a45 45 0 1 0 0 90 45 45 0 0 0 0-90zm0 10a35 35 0 1 1 0 70 35 35 0 0 1 0-70zm0 8l8 16 18 3-13 13 3 18-16-8-16 8 3-18-13-13 18-3z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_pin = {
+	label: 'pin, thumbtack',
+	paths: [
+		{
+			d: 'M50 5a18 18 0 0 0-18 18c0 7 4 13 9 17l-6 18h30l-6-18c5-4 9-10 9-17a18 18 0 0 0-18-18zm-5 63h10v27h-10z',
+		},
+	],
+} satisfies SvgData;
+
+// --- System ---
+
+export const icon_terminal = {
+	label: 'terminal, command prompt',
+	paths: [
+		{
+			d: 'M10 10h80v80h-80zm8 8v64h64v-64zm7 10l20 20-20 20-7-7 13-13-13-13zm25 35h30v8h-30z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_workspace = {
+	label: 'workspace, grid',
+	paths: [
+		{
+			d: 'M10 10h35v35h-35zm45 0h35v35h-35zm-45 45h35v35h-35zm45 0h35v35h-35z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_idea = {
+	label: 'idea, lightbulb',
+	paths: [
+		{
+			d: 'M50 5a30 30 0 0 0-30 30c0 12 7 22 15 28v12a5 5 0 0 0 5 5h20a5 5 0 0 0 5-5v-12c8-6 15-16 15-28a30 30 0 0 0-30-30zm0 12a18 18 0 0 1 18 18c0 8-5 15-12 20v8h-12v-8c-7-5-12-12-12-20a18 18 0 0 1 18-18zm-8 70h16v6h-16z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Communication ---
+
+export const icon_ping = {
+	label: 'ping, circular arrow',
+	paths: [
+		{
+			d: 'M50 10a40 40 0 1 1-28 12l8 8a28 28 0 1 0 20-8v12l-20-20 20-20v16z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_heartbeat = {
+	label: 'heartbeat, pulse line',
+	paths: [
+		{
+			d: 'M5 50h25l8-20 12 40 12-40 8 20h25v8h-30l-5-12-12 40-12-40-5 12h-26z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_response = {
+	label: 'response, reply arrow',
+	paths: [{d: 'M40 20l-30 30 30 30v-20h30c10 0 15 5 15 15v15h10v-15c0-20-10-30-25-30h-30z'}],
+} satisfies SvgData;
+
+export const icon_session = {
+	label: 'session, hexagon with dot',
+	paths: [
+		{
+			d: 'M50 5l40 23v44l-40 23-40-23v-44zm0 12l-30 17v34l30 17 30-17v-34zm0 20a13 13 0 1 0 0 26 13 13 0 0 0 0-26z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Action Types ---
+
+export const icon_action_type_local_call = {
+	label: 'local call, right arrow',
+	paths: [{d: 'M15 40h40v-15l30 25-30 25v-15h-40z'}],
+} satisfies SvgData;
+
+export const icon_action_type_remote_notification = {
+	label: 'remote notification, arrow with dot',
+	paths: [
+		{
+			d: 'M15 40h30v-15l30 25-30 25v-15h-30zm55-15a10 10 0 1 0 0 20 10 10 0 0 0 0-20z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_action_type_request_response = {
+	label: 'request response, bidirectional arrows',
+	paths: [
+		{
+			d: 'M30 15l-20 20 20 20v-12h40v-16h-40zm40 30v12h-40v16h40v12l20-20z',
+		},
+	],
+} satisfies SvgData;
+
+// --- Links ---
+
+export const icon_external_link = {
+	label: 'external link, arrow out of box',
+	paths: [
+		{
+			d: 'M70 5h25v25l-10-10-30 30-7-7 30-30zm-50 10h30v10h-30v50h50v-30h10v35a5 5 0 0 1-5 5h-60a5 5 0 0 1-5-5v-60a5 5 0 0 1 5-5z',
+		},
+	],
+} satisfies SvgData;
+
+export const icon_arrow_right = {
+	label: 'arrow right',
+	paths: [{d: 'M10 45h55v-15l25 20-25 20v-15h-55z'}],
+} satisfies SvgData;
+
+export const icon_arrow_left = {
+	label: 'arrow left',
+	paths: [{d: 'M90 45h-55v-15l-25 20 25 20v-15h55z'}],
+} satisfies SvgData;
+
+// --- Backend ---
+
+export const icon_backend = {
+	label: 'backend, server',
+	paths: [
+		{
+			d: 'M15 10h70v25h-70zm8 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm15 0h30v10h-30zm-23 23h70v25h-70zm8 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm15 0h30v10h-30zm-23 23h70v25h-70zm8 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm15 0h30v10h-30z',
+		},
+	],
+} satisfies SvgData;
---
+++ src/routes/docs/icons/+page.svelte
@@ -0,0 +1,256 @@
+<script lang="ts">
+	import {get_tome_by_name} from '$lib/tome.js';
+	import TomeContent from '$lib/TomeContent.svelte';
+	import TomeLink from '$lib/TomeLink.svelte';
+	import Svg from '$lib/Svg.svelte';
+	import Glyph from '$lib/Glyph.svelte';
+	import {
+		icon_unknown,
+		icon_important,
+		icon_info,
+		icon_add,
+		icon_remove,
+		icon_delete,
+		icon_clear,
+		icon_restore,
+		icon_cancel,
+		icon_drag,
+		icon_copy,
+		icon_paste,
+		icon_reset,
+		icon_refresh,
+		icon_connect,
+		icon_disconnect,
+		icon_retry,
+		icon_placeholder,
+		icon_send,
+		icon_play,
+		icon_pause,
+		icon_checkmark,
+		icon_xmark,
+		icon_xmark_heavy,
+		icon_download,
+		icon_error,
+		icon_chevron_up,
+		icon_chevron_right,
+		icon_chevron_down,
+		icon_chevron_left,
+		icon_double_chevron_left,
+		icon_double_chevron_right,
+		icon_edit,
+		icon_sort,
+		icon_chat,
+		icon_thread,
+		icon_turn,
+		icon_file,
+		icon_list,
+		icon_directory,
+		icon_create_file,
+		icon_create_folder,
+		icon_prompt,
+		icon_part,
+		icon_provider,
+		icon_model,
+		icon_action,
+		icon_view,
+		icon_preview,
+		icon_log,
+		icon_tab,
+		icon_project,
+		icon_capability,
+		icon_settings,
+		icon_domain,
+		icon_page,
+		icon_space,
+		icon_desk,
+		icon_pin,
+		icon_terminal,
+		icon_workspace,
+		icon_idea,
+		icon_ping,
+		icon_heartbeat,
+		icon_response,
+		icon_session,
+		icon_action_type_local_call,
+		icon_action_type_remote_notification,
+		icon_action_type_request_response,
+		icon_external_link,
+		icon_arrow_right,
+		icon_arrow_left,
+		icon_backend,
+	} from '$lib/icons.js';
+
+	const LIBRARY_ITEM_NAME = 'icons';
+
+	const tome = get_tome_by_name(LIBRARY_ITEM_NAME);
+
+	const icon_entries: Array<{name: string; glyph: string; data: import('$lib/Svg.svelte').SvgData}> =
+		[
+			{name: 'unknown', glyph: '\u2047', data: icon_unknown},
+			{name: 'important', glyph: '\u2048', data: icon_important},
+			{name: 'info', glyph: '\u24D8', data: icon_info},
+			{name: 'add', glyph: '\u271B', data: icon_add},
+			{name: 'remove', glyph: '\uD83D\uDDD9', data: icon_remove},
+			{name: 'delete', glyph: '\u2327', data: icon_delete},
+			{name: 'clear', glyph: '\u232B', data: icon_clear},
+			{name: 'restore', glyph: '\u293B', data: icon_restore},
+			{name: 'cancel', glyph: '\u29B8', data: icon_cancel},
+			{name: 'drag', glyph: '\u2261', data: icon_drag},
+			{name: 'copy', glyph: '\u29C9', data: icon_copy},
+			{name: 'paste', glyph: '\u238C', data: icon_paste},
+			{name: 'reset', glyph: '\u293A', data: icon_reset},
+			{name: 'refresh', glyph: '\u27F3', data: icon_refresh},
+			{name: 'connect', glyph: '\u2B4D', data: icon_connect},
+			{name: 'disconnect', glyph: '\u229D', data: icon_disconnect},
+			{name: 'retry', glyph: '\u21BA', data: icon_retry},
+			{name: 'placeholder', glyph: '\u21B3', data: icon_placeholder},
+			{name: 'send', glyph: '\u2BA5', data: icon_send},
+			{name: 'play', glyph: '\u25B6\uFE0E', data: icon_play},
+			{name: 'pause', glyph: '\u2759\u2759', data: icon_pause},
+			{name: 'checkmark', glyph: '\u2713', data: icon_checkmark},
+			{name: 'xmark', glyph: '\u2717', data: icon_xmark},
+			{name: 'xmark_heavy', glyph: '\u2718', data: icon_xmark_heavy},
+			{name: 'download', glyph: '\u2B73', data: icon_download},
+			{name: 'error', glyph: '\u203C', data: icon_error},
+			{name: 'chevron_up', glyph: '\u2B9D', data: icon_chevron_up},
+			{name: 'chevron_right', glyph: '\u2B9E', data: icon_chevron_right},
+			{name: 'chevron_down', glyph: '\u2B9F', data: icon_chevron_down},
+			{name: 'chevron_left', glyph: '\u2B9C', data: icon_chevron_left},
+			{name: 'double_chevron_left', glyph: '\u00AB', data: icon_double_chevron_left},
+			{name: 'double_chevron_right', glyph: '\u00BB', data: icon_double_chevron_right},
+			{name: 'edit', glyph: '\u270E', data: icon_edit},
+			{name: 'sort', glyph: '\u21C5', data: icon_sort},
+			{name: 'chat', glyph: '\u2338', data: icon_chat},
+			{name: 'thread', glyph: '\u2637', data: icon_thread},
+			{name: 'turn', glyph: '\u238D', data: icon_turn},
+			{name: 'file', glyph: '\u29C8', data: icon_file},
+			{name: 'list', glyph: '\u25A4', data: icon_list},
+			{name: 'directory', glyph: '\u25A6', data: icon_directory},
+			{name: 'create_file', glyph: '\uD83D\uDDCE', data: icon_create_file},
+			{name: 'create_folder', glyph: '\uD83D\uDDC1', data: icon_create_folder},
+			{name: 'prompt', glyph: '\u2307', data: icon_prompt},
+			{name: 'part', glyph: '\u250A', data: icon_part},
+			{name: 'provider', glyph: '\u2A15', data: icon_provider},
+			{name: 'model', glyph: '\u229B', data: icon_model},
+			{name: 'action', glyph: '\u237E', data: icon_action},
+			{name: 'view', glyph: '\u235C', data: icon_view},
+			{name: 'preview', glyph: '\u29BE', data: icon_preview},
+			{name: 'log', glyph: '\u2399', data: icon_log},
+			{name: 'tab', glyph: '\u26F6', data: icon_tab},
+			{name: 'project', glyph: '\u232C', data: icon_project},
+			{name: 'capability', glyph: '\u29B0', data: icon_capability},
+			{name: 'settings', glyph: '\u26EE', data: icon_settings},
+			{name: 'domain', glyph: '\u27E1', data: icon_domain},
+			{name: 'page', glyph: '\u233A', data: icon_page},
+			{name: 'space', glyph: '\u2302', data: icon_space},
+			{name: 'desk', glyph: '\u235F', data: icon_desk},
+			{name: 'pin', glyph: '\u23CD', data: icon_pin},
+			{name: 'terminal', glyph: '\u2328', data: icon_terminal},
+			{name: 'workspace', glyph: '\u229E', data: icon_workspace},
+			{name: 'idea', glyph: '\u2306', data: icon_idea},
+			{name: 'ping', glyph: '\u2940', data: icon_ping},
+			{name: 'heartbeat', glyph: '\u223D', data: icon_heartbeat},
+			{name: 'response', glyph: '\u2B91', data: icon_response},
+			{name: 'session', glyph: '\u23E3', data: icon_session},
+			{name: 'action_type_local_call', glyph: '\u2933', data: icon_action_type_local_call},
+			{
+				name: 'action_type_remote_notification',
+				glyph: '\u2959',
+				data: icon_action_type_remote_notification,
+			},
+			{
+				name: 'action_type_request_response',
+				glyph: '\u296E',
+				data: icon_action_type_request_response,
+			},
+			{name: 'external_link', glyph: '\uD83E\uDC75', data: icon_external_link},
+			{name: 'arrow_right', glyph: '\u2192', data: icon_arrow_right},
+			{name: 'arrow_left', glyph: '\u2190', data: icon_arrow_left},
+			{name: 'backend', glyph: '\uD83D\uDF22', data: icon_backend},
+		];
+</script>
+
+<TomeContent {tome}>
+	<section>
+		<p>
+			SVG icon replacements for Unicode character glyphs. Each icon is available as <TomeLink
+				name="Svg"
+			/> data. The character glyph is shown on the left, the SVG icon on the right.
+		</p>
+	</section>
+	<section>
+		<div class="icons">
+			{#each icon_entries as entry (entry.name)}
+				<div class="icon_entry">
+					<div class="icon_name">{entry.name}</div>
+					<div class="icon_comparison">
+						<div class="icon_cell">
+							<div class="icon_frame">
+								<Glyph glyph={entry.glyph} size="var(--icon_size_xl2)" />
+							</div>
+							<small>char</small>
+						</div>
+						<div class="icon_cell">
+							<div class="icon_frame">
+								<Svg data={entry.data} size="var(--icon_size_xl2)" shrink={false} />
+							</div>
+							<small>svg</small>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</section>
+</TomeContent>
+
+<style>
+	.icons {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--space_md);
+		justify-content: center;
+	}
+
+	.icon_entry {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--space_xs);
+		padding: var(--space_md);
+		border-bottom: 1px solid var(--border_color, var(--text_10));
+	}
+
+	.icon_name {
+		font-family: var(--font_family_mono);
+		font-size: var(--font_size_sm);
+	}
+
+	.icon_comparison {
+		display: flex;
+		gap: var(--space_xl2);
+		align-items: center;
+	}
+
+	.icon_cell {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--space_xs);
+	}
+
+	.icon_frame {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: var(--icon_size_xl3);
+		height: var(--icon_size_xl3);
+		flex-shrink: 0;
+		background-color: var(--shade_20);
+		border-radius: var(--radius_xs);
+	}
+
+	small {
+		font-size: var(--font_size_xs);
+		opacity: 0.6;
+	}
+</style>
