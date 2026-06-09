f80b3fbbbffcd379cb5166d9e19b1d80fbd0b34c
Ryan Atkinson <mail@ryanatkn.com>
2026-06-09 08:04:54 -0400

    wip

--- src/routes/docs/Teleport/+page.svelte
+++ src/routes/docs/Teleport/+page.svelte
@@ -4,14 +4,13 @@
 	import {tome_get_by_slug} from '$lib/tome.js';
 	import Teleport from '$lib/Teleport.svelte';
 	import TomeContent from '$lib/TomeContent.svelte';
-	import DeclarationLink from '$lib/DeclarationLink.svelte';
 
 	const TOME_SLUG = 'Teleport';
 	const tome = tome_get_by_slug(TOME_SLUG);
 
 	let swap = $state.raw(true);
-	let teleport_1: HTMLElement | undefined = $state.raw();
-	let teleport_2: HTMLElement | undefined = $state.raw();
+	let port1: HTMLElement | undefined = $state.raw();
+	let port2: HTMLElement | undefined = $state.raw();
 </script>
 
 <!-- eslint-disable svelte/no-useless-mustaches -->
@@ -21,8 +20,7 @@
 		<p>
 			Relocates elements in the DOM, in the rare cases that's useful and the best solution. (like
 			when you need to escape overflow containment or avoid inheriting styles without restructuring
-			the component tree) The API is restricted to manipulating DOM elements, so it does not work
-			with SSR.
+			the component tree) Does not work with SSR.
 		</p>
 		<aside>
 			<p>Use only when necessary or fun.</p>
@@ -32,12 +30,12 @@
 		<!-- TODO make this a generic data-driven helper -->
 		<Code lang="ts" content={`import Teleport from '@fuzdev/fuz_ui/Teleport.svelte';`} />
 		<Code
-			content={`<Teleport to={swap ? teleport_1 : teleport_2}>
+			content={`<Teleport to={swap ? port1 : port2}>
 	🐰
 </Teleport>
-<div class="teleports">
-	<div class="panel" bind:this={teleport_1} />
-	<div class="panel" bind:this={teleport_2} />
+<div class="ports">
+	<div bind:this={port1} class="panel" />
+	<div bind:this={port2} class="panel" />
 </div>
 <button onclick={() => (swap = !swap)}>
 	teleport the bunny
@@ -46,28 +44,22 @@
 	</section>
 	<section>
 		<!-- TODO how to do this with the `state` API? the `bind` in particular -- maybe make them stores? -->
-		<Teleport to={swap ? teleport_1 : teleport_2}>🐰</Teleport>
-		<div class="teleports">
-			<div class="panel" bind:this={teleport_1}></div>
-			<div class="panel" bind:this={teleport_2}></div>
+		<Teleport to={swap ? port1 : port2}>🐰</Teleport>
+		<div class="ports">
+			<div bind:this={port1} class="panel"></div>
+			<div bind:this={port2} class="panel"></div>
 		</div>
 		<button type="button" onclick={() => (swap = !swap)}> teleport the bunny </button>
-	</section>
-	<section>
-		<aside>
-			The <DeclarationLink name="Dialog" /> previously used this but it no longer needs it after migrating
-			to the native <code>&lt;dialog></code>. It's currently unused in fuz_ui and may be removed.
-		</aside>
 	</section>
 </TomeContent>
 
 <style>
-	.teleports {
+	.ports {
 		display: flex;
 		gap: var(--space_sm);
 		margin-bottom: var(--space_sm);
 	}
-	.teleports > div {
+	.ports > div {
 		width: var(--space_xl8);
 		height: var(--space_xl8);
 		font-size: var(--font_size_xl3);
