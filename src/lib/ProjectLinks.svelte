7362ec0a13d0b567e4a486fea6af525b78728ddf
Ryan Atkinson <mail@ryanatkn.com>
2026-08-11 15:31:11 -0400

    fix: tweak layout for items n `ProjectLinks`

--- src/lib/ProjectLinks.svelte
+++ src/lib/ProjectLinks.svelte
@@ -25,10 +25,10 @@
 {#snippet package_thumbnail(project_item: ProjectItem)}
 	{@const counts = series_by_name.get(project_item.name)}
 	<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
-	<a class="thumbnail row shade_00 px_md py_xs border_radius_sm mb_lg" href={project_item.url}>
+	<a class="thumbnail row align-items:start shade_00 px_md py_xs border_radius_sm mb_lg" href={project_item.url}>
 		<Svg shrink={false} data={project_item.logo} size="var(--icon_size_lg)" />
 		<div class="pl_lg width_atmost_sm">
-			<div class="thumbnail-name row align-items:start {project_item.color_class}">
+			<div class="thumbnail-name row {project_item.color_class}">
 				{project_item.name}
 				{#if counts}
 					{@const stats_label = label_by_name.get(project_item.name)}
